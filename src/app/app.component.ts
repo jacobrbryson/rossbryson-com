a7d42483158de49723166216d6710334ac8f45dc
Ross Bryson <jacobrbryson@gmail.com>
2025-07-07 13:04:25 -0400

    RB-0: Testing pipeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common'; // <-- Import CommonModule
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
 })
 export class AppComponent {
-  title = 'rossbryson-com';
+  title = 'Portfolio';
+  currentDate = new Date(); // <-- Add this line
 }
