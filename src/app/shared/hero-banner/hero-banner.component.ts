852e2b932e411b3ed327ec03c545c1e725f5434d
Ross Bryson <jacobrbryson@gmail.com>
2025-09-06 22:43:50 -0400

    RB-0: Fixed call to action

--- src/app/shared/hero-banner/hero-banner.component.ts
+++ src/app/shared/hero-banner/hero-banner.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { NgIconComponent, provideIcons } from '@ng-icons/core'; // Don't forget to import NgIconComponent
+import { RouterLink } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { heroArrowRight } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-hero-banner',
   standalone: true,
-  imports: [CommonModule, NgIconComponent], // Include NgIconComponent if you plan to use icons in the button
+  imports: [CommonModule, NgIconComponent, RouterLink],
   templateUrl: './hero-banner.component.html',
   styleUrl: './hero-banner.component.css',
   providers: [
